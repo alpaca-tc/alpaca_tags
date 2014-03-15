10c3af5bdd82b8628a0643519f0e962eebefb049
alpaca-tc <alpaca-tc@alpaca.tc>
2014-03-15 16:44:19 +0900

    Remove unused files

--- spec/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-require 'fuubar'
-require './lib/alpaca_tags'
-
-SPEC_ROOT = File.expand_path(File.join(File.dirname(__FILE__)))
-
-Rspec.configure do |config|
-  config.order = 'random'
-end
